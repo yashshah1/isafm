d77a3d275fe6c02eda9a0fa6678f6ad2bcc72b8e
Yash Shah <yash.shah@bigbinary.com>
2021-05-01 21:00:37 +0530

    added utils

---
+++ src/utils/url.js
@@ -0,0 +1,19 @@
+export const generateQueryString = (data) => {
+  return Object.entries(data)
+    .map(([k, v]) => (v.trim().length > 0 ? `${k}=${v}` : ""))
+    .join("&");
+};
+
+export const parseQueryString = (s) => {
+  const obj = {};
+  if (s.length <= 1) return { state: 21, district: 363, age: 21 };
+
+  const queryStringParams = s.slice(1).split("&");
+  queryStringParams.forEach((s) => {
+    if (s.trim().length === 0) return;
+    const [k, v] = s.split("=");
+    if (v.trim().length > 0) obj[k] = v;
+  });
+
+  return obj;
+};
